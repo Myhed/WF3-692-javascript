879cd4bd1b53c9c24569e1a8b169c5038946d0cf
= <=>
2021-01-27 10:18:50 +0100

    add notion fonctions

--- js/aide.js
+++ js/aide.js
@@ -45,7 +45,7 @@ window.onload = function(){
     var f = true || false;
 
     // Les boucles
-        // Boucle for fonctionnement 
+        // Boucle for fonctionnement :, 
         for(var i = 0; i <= 10; i = i + 1){
             console.log("boucle for: ",i)
         }
@@ -108,5 +108,58 @@ window.onload = function(){
         // supprimer le première élément d'un tableau
         tableau3.shift();
         console.log("la méthode shift supprime poire au début du tableau: ", tableau3);
+
+        // Les fonctions
+        function sayHelloWorld(){
+            var x = "helloWorld";
+            return x;
+        }
+        // lancer la fonction nommer
+        var monResultat = sayHelloWorld();
+        console.log("function nommer: ", monResultat);
+        
+        var maVariableFunction = function(){
+            return "Hello World!";
+        }
+        // Lancer la fonction annonyme
+        var  monResultat2 = maVariableFunction();
+        var monResultat3 = maVariableFunction();
+        console.log("function annonyme: ", monResultat2);
+        console.log("function annonyme: ", monResultat3);
+
+        function faireCrepe(quantityOeuf, quantityFarine, quantityLait){
+            console.log("ingrédient recette: ", "," , quantityOeuf, "," ,quantityFarine ,",", quantityLait);
+        }
+        // appelation la fonction faireCrepe avec une chaine de caractère en paramètre
+        faireCrepe("3", "4 kg", "2 brique"); 
+        // appelation de faireCrepe avec des nombres en parametre
+        faireCrepe(3, 4, 2);       
+        // appelation de faireCrepe avec des objets en parametre
+        faireCrepe({
+            quantite: 3
+        }, {
+            qauntite: 4
+        }, {
+            quantite: 2
+        });
+        // appelation de faireCrepe avec des tableau
+        faireCrepe([3], [4], [2]); 
+
+        // Convertir une chaine de caractère en nombre
+        var nombreString = "10";
+
+        console.log("type: ",typeof nombreString, "value: ", nombreString);
+
+        var nombreNumber = parseInt(nombreString, 10);
+        
+        console.log("type: ",typeof nombreNumber, "value: ", nombreNumber);
+
+        // nombreString reste une chaine de caractère, elle n'a pas changé.
+        console.log('nombreString: ', typeof nombreString)
+
+        // Convertir un nombre en chaine de caractère
+        var reConvertNombreNumberToString = nombreNumber.toString()
+        console.log("type: ", typeof reConvertNombreNumberToString, "value: ", reConvertNombreNumberToString);
+        
     
 }
--- js/main.js
+++ js/main.js
@@ -1,23 +1,54 @@
 window.onload = function(){
-    var obj1 = {
-        nomFruit: "banane",
-        quantity: 9,
-        prixUniter: 1.20
+    // Les fonctions
+    function sayHelloWorld(){
+        var x = "helloWorld";
+        return x;
     }
-    var tableauObj = [{
-        nomFruit: "banane",
-        quantity: 9,
-        prixUniter: 1.20
-    },
-    {
-        nomFruit: "orange",
-        quantity: 9,
-        prixUniter: 1.10
-    },
-    {
-        nomFruit: "ananas",
-        quantity: 9,
-        prixUniter: 1.30
-    },
-]
+    // lancer la fonction nommer
+    var monResultat = sayHelloWorld();
+    console.log("function nommer: ", monResultat);
+    
+    var maVariableFunction = function(){
+        return "Hello World!";
+    }
+    // Lancer la fonction annonyme
+    var  monResultat2 = maVariableFunction();
+    var monResultat3 = maVariableFunction();
+    console.log("function annonyme: ", monResultat2);
+    console.log("function annonyme: ", monResultat3);
+
+    function faireCrepe(quantityOeuf, quantityFarine, quantityLait){
+        console.log("ingrédient recette: ", "," , quantityOeuf, "," ,quantityFarine ,",", quantityLait);
+    }
+    // appelation la fonction faireCrepe avec une chaine de caractère en paramètre
+    faireCrepe("3", "4 kg", "2 brique"); 
+    // appelation de faireCrepe avec des nombres en parametre
+    faireCrepe(3, 4, 2);       
+    // appelation de faireCrepe avec des objets en parametre
+    faireCrepe({
+        quantite: 3
+    }, {
+        qauntite: 4
+    }, {
+        quantite: 2
+    });
+    // appelation de faireCrepe avec des tableau
+    faireCrepe([3], [4], [2]); 
+
+    // Convertir une chaine de caractère en nombre
+    var nombreString = "10";
+
+    console.log("type: ",typeof nombreString, "value: ", nombreString);
+
+    var nombreNumber = parseInt(nombreString, 10);
+    
+    console.log("type: ",typeof nombreNumber, "value: ", nombreNumber);
+
+    // nombreString reste une chaine de caractère, elle n'a pas changé.
+    console.log('nombreString: ', typeof nombreString)
+
+    // Convertir un nombre en chaine de caractère
+    var reConvertNombreNumberToString = nombreNumber.toString()
+    console.log("type: ", typeof reConvertNombreNumberToString, "value: ", reConvertNombreNumberToString);
+
 }
