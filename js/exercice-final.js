a3b6a159ec1962a6a68953247ac74a7c9e2bc53a
= <=>
2021-02-10 15:06:14 +0100

    add old version javascript procedural

--- js/exercice-final.js
+++ js/exercice-final.js
@@ -1,111 +1,70 @@
 window.onload = function(){
     // Lien à utiliser
     const defaultLinkCard = 'https://via.placeholder.com/';
-    let textCard = "?text=MisteryCard";
+    const defaultText = "MisteryCard";
+
+    let textCard = "?text=";
     let widthCard = "175"; 
+    let firstRandomIndex = 0;
+    let secondRandomIndex = 0;
+    const colorCards = [];
     const nameCards = ["As","roi","dame","valet"];
     const typeCartes = ["Trefle", "Carreau", "Coeur", "Pique"];
     const cardWithName = [];
     const cardWithType = [];
 
-    for(let i = 0; i < typeCartes.length; i++){
-        cardWithName[i] = [];
-        for(let j = 0; j < nameCards.length; j++){
-            cardWithName[i][j] = `${typeCartes[i]} ${nameCards[j]}` 
+    for(let v = 0; v < typeCartes.length; v++){
+        cardWithName[v] = [];
+        for(h = 0; h < nameCards.length; h++){
+            cardWithName[v][h] = typeCartes[v] + ' ' + nameCards[h];
         }
     }
 
-    for(let i = 0; i < typeCartes.length; i++){
-        cardWithType[i] = [];
-        for(let j = 2; j <= 10; j++){
-            cardWithType[i][j-2] = `${typeCartes[i]} ${j}` 
+    for(let x = 0; x < typeCartes.length; x++){
+        cardWithType[x] = [];
+        for(m = 2; m <= 10; m++){
+            cardWithType[x][m-2] = typeCartes[x] + ' ' + m
         }
     }
 
-   const gameCards = cardWithName.concat(cardWithType);
-
-    // Initialisation des 32 carte
-    Array(32).fill(null).forEach((__, index) => {
-        // Créations des différentes cartes
+    for(let j = 0; j < 51; j++){
+        colorCards[j] = "";
+        for(let k = 0; k < 5; k++){
+            colorCards[j] += (Math.floor(Math.random() * 16)).toString(16);
+        }
+    }
+    console.log('cardWithName: ', cardWithName);
+    console.log('cardWithType: ', cardWithType);
+    const gameCards = cardWithName.concat(cardWithType);
+    console.log("gameCards is concat array cardWithName and cardWithType: ", gameCards);
+    
+    for(let i = 0; i <= 51; i++){
+         // Créations des différentes cartes
         const divElement = document.createElement('div');
         const imgElement = document.createElement('img');
         // Assign to card his attributes
         divElement.setAttribute('class', 'card');
-        divElement.setAttribute('id', `card${index+1}`);
+        divElement.setAttribute('id', `card${i+1}`);
         // Assign to img his attributes
-        imgElement.setAttribute('src', defaultLinkCard+widthCard+textCard);
+        imgElement.setAttribute('src', defaultLinkCard+widthCard+textCard+defaultText);
         // set child to div
+        firstRandomIndex = Math.floor(Math.random() * gameCards.length);
+        secondRandomIndex = Math.floor(Math.random() * gameCards[firstRandomIndex].length);
+        const nameCard = gameCards[firstRandomIndex][secondRandomIndex]
         divElement.appendChild(imgElement);
-        document.body.appendChild(divElement);
-    });
-
-    // affecte à listener to each card
-
-    const cards = document.querySelectorAll('.card');
-    const colorsCards = Array(cards.length).fill(null).map((__, index) => {
-        return Array(6).fill(null).reduce((acc, item) => {
-            item = (Math.floor(Math.random() * 16)).toString(16);
-            acc += item;
-            return acc;
-        }, "");
-    });
-
-    cards.forEach((card, index) => {
-        card.addEventListener('click', function(){
-            const firstRandomIndex = Math.floor(Math.random() * gameCards.length);
-            const secondRandomIndex = Math.floor(Math.random() * gameCards[firstRandomIndex].length);
-            console.log(gameCards[firstRandomIndex][secondRandomIndex]);
-            this.children[0].src = defaultLinkCard + widthCard + '/' + colorsCards[index]+ '/' + 'FFFFFF' + '/'+ `?text=${gameCards[firstRandomIndex][secondRandomIndex]}`;
+        imgElement.addEventListener('click', function(e){
+            e.target.src = defaultLinkCard + widthCard + '/' + colorCards[i] + '/' + 'FFFFFF' + '/' + textCard + nameCard;
             gameCards[firstRandomIndex].splice(secondRandomIndex, 1);
+            console.log("removed cardsName on array", gameCards)
             if(gameCards[firstRandomIndex].length === 0){
                 gameCards.splice(firstRandomIndex, 1);
             }
-            console.log("gameCards: ", gameCards);
-        }, false);
+        });
 
-        card.addEventListener('dblclick', function(){
-            this.children[0].src = defaultLinkCard + widthCard + '/' + textCard;
-        }, false);
-    });
+        imgElement.addEventListener('dblclick', function(e){
+            e.target.src = defaultLinkCard + widthCard + '/' + textCard + defaultText
+        });
 
-
-    // const generateCard52 = typeCartes.map(typeCarte => {
-    //    return Array(14).fill({}).reduce((acc, item, index) => {
-    //         if(typeof item[typeCarte] !== "undefined"){
-    //             const obj = {}
-    //             item[typeCarte][index] = Object.assign(obj, {typeCarte});
-    //             if(index <= 9 ){
-    //                 item[typeCarte][index] = Object.assign(obj, {nombreCarte: index+1});
-    //             }else{
-    //                 console.log(index);
-    //                 item[typeCarte][index] = Object.assign(obj, {nomCarte: `${nomCartes[index%nomCartes.length]}`});
-    //             }
-    //         }else{
-    //             item[typeCarte] = {};
-    //         }
-    //         acc = item;
-    //         return acc;
-    //     }, {});
-    // });
-
-//     const generateCard32 = typeCartes.map(typeCarte => {
-//         return Array(9).fill({}).reduce((acc, item, index) => {
-//              if(typeof item[typeCarte] !== "undefined"){
-//                  const obj = {}
-//                  item[typeCarte][index] = Object.assign(obj, {typeCarte});
-//                  if(index >= 5 ){
-//                      item[typeCarte][index] = Object.assign(obj, {nombreCarte: index+2});
-//                  }else{
-//                      item[typeCarte][index] = Object.assign(obj, {nomCarte: `${nomCartes[index%nomCartes.length]}`});
-//                     //  console.log(index);
-//                  }
-//              }else{
-//                  item[typeCarte] = {};
-//              }
-//              acc = item;
-//              return acc;
-//          }, {});
-//      });
-
-//     console.log(generateCard32);
+        document.body.appendChild(divElement);
+    }
 }
